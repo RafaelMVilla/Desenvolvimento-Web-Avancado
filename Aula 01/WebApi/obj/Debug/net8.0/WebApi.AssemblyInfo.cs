ddada707316ad21252a4ee946e0be388bc1ae518
RafaelMVilla <rafael.march.v@gmail.com>
2025-04-02 20:27:51 -0300

    bugdabranch4quaseresolvido

--- Aula 01/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ Aula 01/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c02077943e4a4ddf89fab5578d5bdc03487365b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc3bd3736d02ff17b07cc3637e617fea72e87df")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
